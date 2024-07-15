9104c85c36b5bcf0dad349bfdb0b9acf3146d59e
Tejas Gowda S <tejasgowda713@gmail.com>
2024-07-15 21:28:11 +0530

    Django Jinja Template

--- django_project/WatchShop/homepage/views.py
+++ django_project/WatchShop/homepage/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Watches
 
 # Create your views here.
 
-
 def Home(request):
-    return render(request, "home.html")
+    watches = Watches.objects.all()
+    context = {'watches_t' : watches}
+    return render(request, "home.html", context)
 
 def About(request):
     return render(request, "about.html")
